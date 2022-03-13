ac404d1102a769e325df8b1d91679049062d946f
svdome <svdome@yandex.ru>
2022-03-13 20:13:23 +0300

    "Лекция 71-72. Выполнение задания "Todo list""

---
+++ js/todo.js
@@ -0,0 +1,63 @@
+/* todo.js */
+
+/**
+ * @typedef {{title: String, isCompleted: Boolean, icon: String, category: String, author: Object}} Todo
+ *
+ * @param {object} data
+ */
+export function createTodo(data) {
+	console.log(data);
+	let icon = '⏳';
+	if(data.completed) {
+		icon = '✅';
+	}
+	return {
+		title: data.todoTitle,
+		isCompleted: data.completed,
+		icon: icon,
+		category: data.chosenCategory,
+		author: {
+		  firstName: data.user.first,
+		  lastName: data.user.last,
+		}
+	  };
+};
+
+/**
+ * @param {Todo[]} todos
+ * @param {Todo} todo
+ */
+export function addTodo(todos, todo) {
+	todos.push(todo);
+	return todos;
+};
+
+/**
+ * @param {Todo[]} todos
+ */
+export function countTodos(todos) {
+	return todos.length;
+};
+
+/**
+ * @param {Todo[]} todos
+ */
+export function firstTodoTitle(todos) {
+	return todos[0].title;
+};
+
+/**
+ * @param {Todo[]} todos
+ */
+export function lastTodoTitle(todos) {
+	return todos[todos.length - 1].title;
+};
+
+/**
+ * @param {Todo[]} todos
+ */
+export function exportTitles(todos) {
+	return todos.map(function(todos) {
+		return todos.title;
+	});
+};
